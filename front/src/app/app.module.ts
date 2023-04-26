dcd242c023f3fff71bf35db77948d36ddb44f1ff
Emilio Everduim <everduim@gmail.com>
2023-04-26 19:21:25 +0200

    se crea core y components

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { AcercaComponent } from './components/acerca/acerca.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { GestionComponent } from './components/gestion/gestion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AcercaComponent,
+    ProductosComponent,
+    ContactoComponent,
+    GestionComponent
   ],
   imports: [
     BrowserModule,
